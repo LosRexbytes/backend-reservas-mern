4ab74d2fd6002b3b2ae4ddfe691ed16f5063a497
PerroTechero <204808@unsaac.edu.pe>
2024-11-19 17:26:32 -0500

    desapareciendo advertencias de la conexion a mongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
     });
     console.log('Conexión a MongoDB exitosa');
   } catch (error) {
